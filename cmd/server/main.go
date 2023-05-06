553e47ab8b0e245b29e9a19c25a7539a5b78d317
kodmm <53547701+kodmm@users.noreply.github.com>
2023-05-06 13:27:04 +0900

    11: gRPCにおけるステータスコード

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,8 +13,11 @@ import (
 
 	hellopb "grpc-tutorial/pkg/grpc"
 
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type myServer struct {
@@ -51,9 +54,13 @@ func NewMyServer() *myServer {
 }
 
 func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
-	return &hellopb.HelloResponse{
-		Message: fmt.Sprintf("Hello %s!", req.GetName()),
-	}, nil
+	stat := status.New(codes.Unknown, "unknown error occurred")
+	stat, _ = stat.WithDetails(&errdetails.DebugInfo{
+		Detail: "detail reason of err",
+	})
+	err := stat.Err()
+
+	return nil, err
 }
 
 func (s *myServer) HelloServerStream(req *hellopb.HelloRequest, stream hellopb.GreetingService_HelloServerStreamServer) error {
